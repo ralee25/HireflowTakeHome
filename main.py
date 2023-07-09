6cd40409ed8907659da3e26d473ae47b9facd89b
Rachel <ralee@g.hmc.edu>
2023-07-09 13:17:20 -0700

    debug not loading notebook

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-from langchain.document_loaders import YoutubeLoader
+import os
+# from langchain.document_loaders import YoutubeLoader
 from langchain.document_loaders.generic import GenericLoader
 from langchain.document_loaders.parsers import OpenAIWhisperParser
 from langchain.document_loaders.blob_loaders.youtube_audio import YoutubeAudioLoader
-import os
-
-from langchain.chains import RetrievalQA
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.chains import RetrievalQA
+# from langchain.vectorstores import FAISS
+# from langchain.chat_models import ChatOpenAI
+# from langchain.embeddings import OpenAIEmbeddings
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.indexes import VectorstoreIndexCreator
 
 os.environ["OPENAI_API_KEY"] = "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"
+
+# working code
+# video_id = "QsYGlZkevEg"
+# loader = YoutubeLoader(video_id)
+# docs=loader.load()
+
+# index = VectorstoreIndexCreator()
+# index = index.from_documents(docs)
+# response = index.query("Where was the show the Last of Us filmed?")
+# print(f"Answer: {response}")
 
 # Two Karpathy lecture videos
 urls = ["https://youtu.be/kCc8FmEb1nY", "https://youtu.be/VMj-3S1tku0"]
@@ -20,30 +30,10 @@ save_dir = "~/Downloads/YouTube"
 
 # Transcribe the videos to text
 loader = GenericLoader(YoutubeAudioLoader(urls, save_dir), OpenAIWhisperParser())
+print(loader)
 docs = loader.load()
 
+YoutubeAudioLoader(urls, save_dir)
+
 # Returns a list of Documents, which can be easily viewed or parsed
-docs[0].page_content[0:500]
-
-# Combine doc
-combined_docs = [doc.page_content for doc in docs]
-text = " ".join(combined_docs)
-
-# Split them
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150)
-splits = text_splitter.split_text(text)
-
-# Build an index
-embeddings = OpenAIEmbeddings()
-vectordb = FAISS.from_texts(splits, embeddings)
-
-# Build a QA chain
-qa_chain = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0),
-    chain_type="stuff",
-    retriever=vectordb.as_retriever(),
-)
-
-# Ask a question!
-query = "Why do we need to zero out the gradient before backprop at each step?"
-qa_chain.run(query)
+print(len(docs))
